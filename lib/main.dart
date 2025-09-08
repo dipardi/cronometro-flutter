7c2924f9c366cf4a0467f7d4f404f08f8731597d
Marcos <marcoseydp@gmail.com>
2025-09-08 19:30:59 -0300

    arrumei o main

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() async {
-  // 1. Garante que o Flutter esteja pronto antes de qualquer outra coisa.
   WidgetsFlutterBinding.ensureInitialized();
 
-  // 2. Chama o método 'init' do nosso serviço para prepará-lo.
   await ServicoNotificacao().init();
 
-  // 3. Inicia o aplicativo.
   runApp(const MyApp());
 }
 
